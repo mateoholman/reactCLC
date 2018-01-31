b7fc7e6b76dc7f090bc13c08da35d6222b9df336
Matthew Holman <mateoholman@gmail.com>
2018-01-30 18:22:48 -0600

    Added material-ui component

---
+++ src/components/material-ui/MUIComp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const MUIComp = () => (
+  <div>
+    <AppBar title="material-ui" />
+  </div>
+);
+
+export default MUIComp;
---
+++ src/components/material-ui/materialUiMock.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MUIComp from './MUIComp';
+
+const materialUIMock = () => (
+  <MuiThemeProvider>
+    <MUIComp />
+  </MuiThemeProvider>
+);
